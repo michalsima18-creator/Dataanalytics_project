d5edaaf7166fa29baf4b449df99202141c16eb76
Michal Šíma <michalsima18@gmail.com>
2026-09-23 18:35:49 +0200

    Add files via upload

---
+++ olympic_country_definitions.py
@@ -0,0 +1,19 @@
+import os 
+import glob
+import pandas 
+import sqlalchemy
+
+SCRIPT_FOLDER = os.path.dirname(__file__) 
+PROJECT_FOLDER = os.path.dirname(SCRIPT_FOLDER)
+SCRIPT_FILENAME = os.path.basename(__file__)  
+DATA_FOLDER = os.path.join(PROJECT_FOLDER,'data')
+
+csv_file_path = os.path.join(DATA_FOLDER,'olympic_country_definitions.csv')
+df = pandas.read_csv(csv_file_path)
+
+df.info()
+print(df)
+
+DBURI = 'mysql+pymysql://root:90Nko5hr@localhost:3317/projekt'
+sqldb = sqlalchemy.create_engine(DBURI)  
+df.to_sql("olympic_country_definitions",sqldb, if_exists='replace')
